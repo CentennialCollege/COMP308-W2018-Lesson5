27d644abf30ea81ab1ec0eeac9c7c37f7f2ff3a1
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-02-20 19:59:42 -0500

    added game model and route files

---
+++ app/routes/games.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// game object created from the Schema / model
+let game = require('../models/games');
+
+/* GET games List page. READ */
+router.get('/', (req, res, next) => {
+  // find all games in the games collection
+  game.find( (err, games) => {
+    if(err) {
+      return console.error(err);
+    }
+    else {
+      res.render('games/index', {
+        title: 'Games',
+        games: games
+      });
+    }
+  });
+});
+
